6b8aff5b54e6790e8d2606d99e5360ea820cb4fd
AJ-Kulundu <jameskulundu@gmail.com>
2022-06-04 20:37:01 +0300

    Loading all characters from the API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next';
+import { Character } from '../api/api';
+import { CharacterType } from '../models/character.interface';
+import LazyImage from '../components/LazyImage';
 
 
-const Home: NextPage = () => {
+export const  getServerSideProps:GetServerSideProps = async (context) => {
+  let result:CharacterType[] ;
+  await Character.getCharacter().then((data) => result=data)
+ 
+return{
+  props:{
+    data:result
+  }
+}
+}
+
+
+const Home: NextPage = ({data}:{data:CharacterType[]}) => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {data.map((data) => (
+          <LazyImage key={data._id} data={data} />
+        ))}
+    
+    </div>
+    </div>
   )
 }
 
