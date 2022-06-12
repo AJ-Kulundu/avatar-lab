06457156326509800de6063486c8214e3179cae5
AJ-Kulundu <jameskulundu@gmail.com>
2022-06-12 16:13:29 +0300

    Updating the padding style for the avatar page

--- pages/avatars.tsx
+++ pages/avatars.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NextPage, GetStaticProps } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
 import { Character } from '../api/api';
 import LazyImage from '../components/LazyImage';
 
-export const getStaticProps:GetStaticProps = async (context) => {
+export const getServerSideProps:GetServerSideProps = async (context) => {
   let result:any;
   await Character.getAvatar().then((data) => result=data);
   return{
@@ -15,9 +15,8 @@ export const getStaticProps:GetStaticProps = async (context) => {
 }
 
 const avatars:NextPage = ({data}:any) => {
-  console.log(data);
   return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="mx-auto max-w-2xl sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {data.map((data) => (
           <LazyImage key={data._id} image={data} />
