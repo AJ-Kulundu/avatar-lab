6944b861fdd951072c3a9770ca887bee71c0be16
AJ-Kulundu <jameskulundu@gmail.com>
2022-06-12 16:12:29 +0300

    Creating the random character interface

---
+++ models/random.interface.ts
@@ -0,0 +1,12 @@
+import { CharacterType } from "./character.interface";
+
+export interface RandomType extends CharacterType {
+hair?:string;
+skin?:string;
+weapon?:string;
+love?:string;
+gender?:string;
+position?:string;
+predecessor?:string;
+first?:string;
+}
