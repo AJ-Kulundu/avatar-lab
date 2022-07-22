c1cbb5013d217c85fe6ed436d35dadd9f2a27441
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-22 08:59:53 +0300

    Fixing the spacing on the pages for mobile phones

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -18,8 +18,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const Characters:NextPage = ({data}:{data:RandomType[]}) => {
   return (
-    <div className="mx-auto max-w-2xl sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className='grid grid-cols-1 gap-y-10 gap-x-6 md:gap-x-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className='grid grid-cols-1 gap-y-10 gap-x-6 md:gap-x-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
         {data.map((data) => (
           <LazyImage key={data._id} image={data} />
         ))}
